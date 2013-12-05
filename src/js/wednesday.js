e68630186c7144cb1bc21ccf084586bcbf8f2b1e
Alex Vince <avince.digital@gmail.com>
2013-12-05 11:27:11 +0000

    Added src directory

---
+++ src/js/wednesday.js
@@ -0,0 +1,13 @@
+( function( $, window, document, undefined )
+{
+
+	'use strict';
+
+	//Localise Globals
+	var console = window.console || undefined;
+
+	//App Namespace
+	var Wednesday = window.Wednesday = window.Wednesday || {}
+
+
+})( jQuery, window, document );
